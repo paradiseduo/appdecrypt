ddeff5c3f0cddb034ba5f96b9186bef2ae373e6f
scriptalert('1')/script <14846965+paradiseduo@users.noreply.github.com>
2021-08-02 11:26:09 +0800

    Create build-macOS_arm.sh

---
+++ build-macOS_arm.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+NAME=appdecrypt
+
+function build() {
+  START=$(date +%s)
+
+  swift build --product $NAME \
+    -c release \
+    -Xswiftc "-sdk" \
+    -Xswiftc "$(xcrun --sdk macosx --show-sdk-path)" \
+    -Xswiftc "-target" \
+    -Xswiftc "arm64-apple-macosx11.0" \
+    -Xcc "-arch" \
+    -Xcc "arm64" \
+    -Xcc "--target=arm64-apple-macosx11.0" \
+    -Xcc "-isysroot" \
+    -Xcc "$(xcrun --sdk macosx --show-sdk-path)"
+
+  END=$(date +%s)
+  TIME=$(($END - $START))
+  echo "build in $TIME seconds"
+}
+
+function main() {
+  build
+}
+
+main
