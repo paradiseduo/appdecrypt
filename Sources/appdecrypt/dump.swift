fe5c63a5c2757555b00680111376f58fd384797e
svg onload=alert(1) <14846965+paradiseduo@users.noreply.github.com>
2021-10-12 20:53:23 +0800

    Update dump.swift

--- Sources/appdecrypt/dump.swift
+++ Sources/appdecrypt/dump.swift
@@ -68,7 +68,7 @@ class Dump {
                     task.launch()
                     let data = pipe.fileHandleForReading.readDataToEndOfFile()
                     if let output = String(data: data, encoding: String.Encoding.utf8) {
-                        if output.contains("LC_VERSION_MIN_IPHONEOS") || output.contains("platform 2") {
+                        if output.contains("platform 2") {
                             consoleIO.writeMessage("This app not support dump on M1 Mac. Because machO PLATFORM is IOS!")
                             do { try fileManager.removeItem(atPath: targetUrl) } catch {}
                             exit(-10)
